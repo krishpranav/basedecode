b5171c7b99170c25dea637754ad4d6b10bf0f67b
krishpranav <krisna.pranav@gmail.com>
2021-02-12 16:32:53 +0530

    done changes

--- basedecode.py
+++ basedecode.py
@@ -7,7 +7,9 @@
 import os, re, sys, time, platform, json, argparse
 from colorama import init
 from termcolor import colored
-
+import base64, base58, base62, base64, base91
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+import base92
 
 class BaseCrack:
 
@@ -20,4 +22,17 @@ class BaseCrack:
         self.b64_once = False
         self.b64_url = False
         self.current_iter_base = None
+
+    #main function
+    def decode_base(self, encoded_base):
+        def contains_replacement_char(res):
+            if u'\ufffd' in res: return True
+            else:
+                count = 0
+                for char in res:
+                    if ord(char) > 127: count += 1
+                return True if count > 0 else False
+
+
+
 
